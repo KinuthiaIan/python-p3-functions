d0e6d7bb0716807b70b1aafbbe04e60deb09e590
Ian Kinuthia 254 <144425644+KinuthiaIan@users.noreply.github.com>
2024-03-05 09:51:17 +0300

    Add files via upload

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,68 @@
 #!/usr/bin/env python3
 
+'''
+  You should be able to call this function with no arguments and see its output in the terminal:
+  greetProgrammer();
+  => "Hello, programmer!"
+  '''
+
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+
+'''
+You should be able to call this function with one argument and see its output in the terminal:
+  greet("Naureen");
+  => "Hello, Naureen!"    
+'''
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+
+'''
+  You should be able to call this function with no arguments and see its output in the terminal:
+  greetWithDefault();
+  => "Hello, programmer!"
+
+  You should also be able to call this function with one argument and see its output in the terminal:
+  greetWithDefault("Sunny");
+  => "Hello, Sunny!"
+'''
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+'''
+  You should be able to call this function with two arguments and get back its return value:
+  const sum = add(1, 2);
+  console.log(sum);
+  => 3
+'''
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+
+'''
+  You should be able to call this function with two arguments and get back its return value:
+  const result = halve(4);
+  console.log(result);
+  => 2
+
+  If the function is called with an argument that isn't a number, it should return null:
+  const result = halve("two")
+  => null
+'''
+
 
 def halve(number):
-    pass
+    if isinstance(number, (int, float)):
+        return number / 2
+    else:
+        return None
